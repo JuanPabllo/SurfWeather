a56c87e78446f5dc334bb08bb99aef7b820265e2
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-09-03 12:14:24 -0300

    config test for pass

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,11 +1,16 @@
 import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+
+jest.mock('axios');
 
 describe('StormGlass clients', () => {
   it('should return the normalized forecast from the StormGlass services', async () => {
     const lat = -33.797876;
     const lng = 152.345674;
 
-    const stormGlass = new StormGlass();
+    axios.get = jest.fn().mockResolvedValue({});
+
+    const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual({});
   });
