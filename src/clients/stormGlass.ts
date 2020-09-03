9c7ede81c5d300aff149aeed329aba75f84c1d0d
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-09-03 12:49:50 -0300

    making params and source fixed

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,10 +1,14 @@
 import { AxiosStatic } from 'axios';
 
 export class StormGlass {
+  readonly stormGlassAPIParams =
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+  readonly stormGlassAPIsource = 'noaa';
+
   constructor(protected request: AxiosStatic) {}
   public async fetchPoints(lat: number, lng: number): Promise<{}> {
     return this.request.get(
-      `https://api.stormglass.io/v2/weather/point?params=swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed&source=noaa&lat=${lat}&lng=${lng}`
+      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPIsource}&end=1592113802&lat=${lat}&lng=${lng}`
     );
   }
 }
