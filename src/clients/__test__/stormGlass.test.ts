19c17c92c4824cba9009913cb614037f8e37a54f
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-09-03 12:52:17 -0300

    using minified files

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,5 +1,7 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import axios from 'axios';
+import stormGlassWeather3hoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3hoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 
 jest.mock('axios');
 
@@ -8,10 +10,10 @@ describe('StormGlass clients', () => {
     const lat = -33.797876;
     const lng = 152.345674;
 
-    axios.get = jest.fn().mockResolvedValue({});
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3hoursFixture);
 
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
-    expect(response).toEqual({});
+    expect(response).toEqual(stormGlassNormalized3hoursFixture);
   });
 });
