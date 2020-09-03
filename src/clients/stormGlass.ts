2332d9bbab10a26a372d8eb8a9ee2cceed931d32
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-09-03 12:13:46 -0300

    config request used the axios

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,10 @@
+import { AxiosStatic } from 'axios';
+
 export class StormGlass {
+  constructor(protected request: AxiosStatic) {}
   public async fetchPoints(lat: number, lng: number): Promise<{}> {
-    return Promise.resolve({});
+    return this.request.get(
+      `https://api.stormglass.io/v2/weather/point?params=swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed&source=noaa&lat=${lat}&lng=${lng}`
+    );
   }
 }
